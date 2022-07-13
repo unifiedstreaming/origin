ca8c1e4ed7925fd2ad8555c887cc5c919fccc78a
Unified Streaming <ops@unified-streaming.com>
2022-07-13 14:14:43 +0000

    Publish 1.11.17

--- docker/origin/entrypoint.sh
+++ docker/origin/entrypoint.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 set -e
 
-# set env vars to defaults if not already set
-if [ -z "$LOG_LEVEL" ]
-  then
-  export LOG_LEVEL=warn
-fi
-
-if [ -z "$LOG_FORMAT" ]
-  then
-  export LOG_FORMAT="%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %D"
-fi
-
-if [ -z "$REMOTE_PATH" ]
-  then
-  export REMOTE_PATH=remote
-fi
-
-# validate required variables are set
+# Validate license key variable is set
 if [ -z "$UspLicenseKey" ] && [ -z "$USP_LICENSE_KEY" ]
   then
   echo >&2 "Error: UspLicenseKey environment variable is required but not set."
@@ -27,7 +11,25 @@ elif [ -z "$UspLicenseKey" ]
   export UspLicenseKey=$USP_LICENSE_KEY
 fi
 
-# set up remote storage proxy config
+# If specified, override default log level and format config
+if [ "$LOG_FORMAT" ]
+then
+  export EXTRA_OPTIONS="$EXTRA_OPTIONS -D LOG_FORMAT"
+fi
+if [ "$LOG_LEVEL" ]
+then
+  export EXTRA_OPTIONS="$EXTRA_OPTIONS -D LOG_LEVEL"
+fi
+
+# Remote storage URL and storage proxy config
+if [ "$REMOTE_STORAGE_URL" ]
+then
+  export EXTRA_OPTIONS="$EXTRA_OPTIONS -D REMOTE_STORAGE_URL"
+  if [ -z "$REMOTE_PATH"]
+  then
+    export REMOTE_PATH=remote
+  fi
+fi
 if [ "$S3_ACCESS_KEY" ]
 then
   export EXTRA_OPTIONS="$EXTRA_OPTIONS -D S3_ACCESS_KEY"
@@ -45,29 +47,18 @@ then
   export EXTRA_OPTIONS="$EXTRA_OPTIONS -D S3_REGION"
 fi
 
-
-
-# remote storage
-if [ "$REMOTE_STORAGE_URL" ]
-  then
-  export EXTRA_OPTIONS="$EXTRA_OPTIONS -D REMOTE_STORAGE_URL"
-fi
-
 # REST API
 if [ "$REST_API_PORT" ]
   then
   export EXTRA_OPTIONS="$EXTRA_OPTIONS -D REST_API_PORT"
 fi
 
-# change Listen 80 to Listen 0.0.0.0:80 to avoid some strange issues when IPv6 is available
+# Change 'Listen 80' to 'Listen 0.0.0.0:80' to avoid some strange issues when IPv6 is available
 /bin/sed -i "s@Listen 80@Listen 0.0.0.0:80@g" /etc/apache2/httpd.conf
-
-# USP license
-echo "$UspLicenseKey" > /etc/usp-license.key
 
 rm -f /run/apache2/httpd.pid
 
-# first arg is `-f` or `--some-option`
+# First arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
   set -- httpd $EXTRA_OPTIONS "$@"
 fi
